a9c8469185163eccea2d979502aaf1e608015164
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-10-16 13:57:56 -0300

    home page banner v1

--- netflix/src/components/Banner.js
+++ netflix/src/components/Banner.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import "./Banner.css"
-import banner from "../img/banner.jpg"
-
 
 export default function Banner() {
+
+    function truncate(string, n) { 
+        return string?.length
+    }
+
+
   return (
     <header className="banner" style={{
         backgroundSize: "cover",
-        backgroundImage: {banner},
+        backgroundImage: `url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxCpHFfswnSmS7dl9vWxzDba6rXcov8rrRX4lgsWMz&s")`,
         backgroundPosition: "center center",
     }}>
         <div className="banner_contents">
-            <h1 className="banner_title">
-                Movie Name
-            </h1>
+            <h1 className="banner_title">Movie Name</h1>
+            <div className="banner_buttons">
+                <button className="banner_button">Play</button>
+                <button className="banner_button">My list</button>
+            </div>
+            <h1 className="banner_description">This is a test discription</h1>
         </div>
+
+        <div className="banner--fadeBottom"/>
     </header>
   )
 }
